99c1755c5b1c632de5f9b759586d7cd4161b134a
jaykit maurya <jaykitmaurya19@gmail.com>
2024-07-09 15:24:02 +0530

    style is given

---
+++ WeatherApp/script.js
@@ -0,0 +1,7 @@
+let info1=document.querySelector('.info');
+
+let search1=document.getElementById("search");
+
+search1.addEventListener('click',()=>{
+    info1.setAttribute('style','display:flex;');
+})
