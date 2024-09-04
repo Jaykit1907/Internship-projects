2c7149215c623ccf2c8a888ce629a3f19f89e83b
jaykit maurya <jaykitmaurya19@gmail.com>
2024-09-04 20:26:53 +0530

    photo top padding increase

--- Portfolio/script3.js
+++ Portfolio/script3.js
@@ -3,16 +3,21 @@
       let menu2=document.querySelector("#menuop");
       let nav3=document.querySelector(".ulelement");
       let cross1=document.querySelector("#cross");
-      let liele1=document.querySelectorAll(".liele");
+//    let liele1=document.querySelectorAll(".liele");
       let resume=document.querySelector("#resumesection");
 
       menu2.addEventListener("click",menuwork);
       function menuwork(){
+        
+           
+   
         resume.setAttribute("style","display:block;");
         console.log("working");
         menu2.setAttribute("style","display:none;");
         cross1.setAttribute("style","display:block");
         nav3.setAttribute("style","display:flex; flex-direction:column;position:fixed;top:100px;");
+
+     
 
         
       }
@@ -21,7 +26,8 @@
         menu2.setAttribute("style","display:block");
         cross1.setAttribute("style","display:none");
         nav3.setAttribute("style","display:none;");
-        liele1.setAttribute("style","height:30px;background-color:white;");
+
+      
       }
       );
         
