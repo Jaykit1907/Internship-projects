d738154f0f6200c61b755aa2df5381d69d6ca94b
jaykit <jaykitmaurya19@gmail.com>
2024-09-22 11:24:50 +0530

    navigation underline added

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,18 +1,5 @@
 
 
-    // let anim=document.getElementById("animation1");
-    // let elm=document.getElementById("element");
-    // setInterval(work,5000);
-    // function work(){
-    //     work2();
-    // var typed = new Typed('#element', {
-    //   strings: ['<i>And I\'m Fontend Developer</i>'],
-    //   typeSpeed: 50,
-    // });
-    // }
-    // function work2(){
-    //     elm.innerHTML="";
-    // }
 
 
     let netflix1=document.getElementById("netflix");
---
+++ script.js
@@ -0,0 +1,11 @@
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+    item.addEventListener('click', function() {
+        // Remove active class from all items
+        navItems.forEach(nav => nav.classList.remove('active'));
+        
+        // Add active class to the clicked item
+        this.classList.add('active');
+    });
+});
