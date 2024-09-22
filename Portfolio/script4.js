1da67c7ae972aff5b338a050c6ac9368a640851c
jaykit <jaykitmaurya19@gmail.com>
2024-09-22 11:24:50 +0530

    navigation underline added

--- Portfolio/script4.js
+++ Portfolio/script4.js
@@ -13,3 +13,17 @@ function updateOnlineStatus() {
   // Listen for online/offline events
   window.addEventListener('online', updateOnlineStatus);
   window.addEventListener('offline', updateOnlineStatus);
+
+
+  
+  const navItems = document.querySelectorAll('.nav-item a');
+
+  // Get the current URL and find the page name (e.g., index.html)
+  const currentPage = window.location.pathname.split("/").pop();
+  
+  navItems.forEach(link => {
+      if (link.getAttribute('href') === currentPage) {
+          link.parentElement.classList.add('active');
+      }
+  });
+  
